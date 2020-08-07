811c1d6c392927a72a904eced5b188911a84d010
asciphx <maxspore@foxmail.com>
2020-08-07 21:42:10 +0800

    Bugfix

--- src/weblogic.js
+++ src/weblogic.js
@@ -1,9 +1,9 @@
 export const W={
-  Qx(req, rep, done){
+  Qx(req, rep, next){
     rep.log.info("Middleware is in `src/weblogic.`")
-    done();
+    next();
   },
   Login(req, rep, next){//Session is not configured at the moment,so it will intercept
-    if (!req.session?.user) {return rep.redirect('/');}done();
+    if (!req.session?.user) {return rep.redirect('/');}next();
   }
 }
